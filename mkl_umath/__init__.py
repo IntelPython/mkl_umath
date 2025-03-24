50d004712a88b9741bf78930a96ece304972401f
Evseniia Komarova <70146207+ekomarova@users.noreply.github.com>
2025-03-24 20:09:35 +0100

    Support mkl_umath in virtual environment out of the box (#48)
    * Support mkl_umath in virtual environment out of the box

--- mkl_umath/__init__.py
+++ mkl_umath/__init__.py
@@ -27,8 +27,12 @@
 Implementation of Numpy universal math functions using Intel(R) MKL and Intel(R) C compiler runtime.
 '''
 
+from . import _init_helper
+
 from ._version import __version__
 
 from ._ufuncs import *
 
 from ._patch import mkl_umath, use_in_numpy, restore, is_patched
+
+del _init_helper

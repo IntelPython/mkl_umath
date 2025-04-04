de3d201a39ba51caaa524d3505a2ef00702d6677
Nikita Grigorian <nikita.grigorian@intel.com>
2025-04-04 15:03:56 -0700

    Update SYCL_INCLUDE_DIR_HINT for newer clang versions

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -5,7 +5,7 @@ set "INCLUDE=%BUILD_PREFIX%\include;%INCLUDE%"
 "%PYTHON%" setup.py clean --all
 set "SKBUILD_ARGS=-G Ninja -- -DCMAKE_C_COMPILER:PATH=icx -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
 
-FOR %%V IN (14.0.0 14 15.0.0 15 16.0.0 16 17.0.0 17) DO @(
+FOR %%V IN (17.0.0 17 18.0.0 18 19.0.0 19 20.0.0 20) DO @(
   REM set DIR_HINT if directory exists
   IF EXIST "%BUILD_PREFIX%\Library\lib\clang\%%V\" (
      SET "SYCL_INCLUDE_DIR_HINT=%BUILD_PREFIX%\Library\lib\clang\%%V"

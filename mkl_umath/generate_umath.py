f1cb6658d38375bb88620b5b21b2de3842facd8c
Vahid Tavanashad <vahid.tavanashad@intel.com>
2025-07-24 09:14:43 -0700

    add support float floor_divide

--- mkl_umath/generate_umath.py
+++ mkl_umath/generate_umath.py
@@ -416,7 +416,12 @@ defdict = {
           TD(inexactvec + cmplxvec),
           ),
 # 'true_divide': aliased to divide
-# 'floor_divide':
+'floor_divide':
+    Ufunc(2, 1, None,
+          docstrings.get('numpy._core.umath.floor_divide'),
+          None,
+          TD(inexactvec),
+          ),
 'divide':
     Ufunc(2, 1, None,
           docstrings.get('numpy._core.umath.divide'),

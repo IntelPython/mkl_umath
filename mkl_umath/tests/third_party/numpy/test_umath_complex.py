a4ee2fc6974a3c738cd5d6d6880156c04541e3e0
Vahid Tavanashad <vahid.tavanashad@intel.com>
2025-07-30 06:18:00 -0700

    add numpy tests

---
+++ mkl_umath/tests/third_party/numpy/test_umath_complex.py
@@ -0,0 +1,3 @@
+# This file includes tests from numpy:
+# https://github.com/numpy/numpy/blob/maintenance/2.3.x/numpy/_core/tests/test_umath_complex.py
+# while mkl_umath.use_in_numpy() is activated to use mkl_umath in Numpy

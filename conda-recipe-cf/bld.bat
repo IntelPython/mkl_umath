cfe4ecaa4cf5fd0df92eced4427b8a20a13660e7
Nikita Grigorian <nikita.grigorian@intel.com>
2026-05-21 02:02:07 -0700

    experimentally add conda-forge workflow

--- conda-recipe-cf/bld.bat
+++ conda-recipe-cf/bld.bat
@@ -2,24 +2,8 @@ REM A workaround for activate-dpcpp.bat issue to be addressed in 2021.4
 set "LIB=%BUILD_PREFIX%\Library\lib;%BUILD_PREFIX%\compiler\lib;%LIB%"
 set "INCLUDE=%BUILD_PREFIX%\include;%INCLUDE%"
 
-"%PYTHON%" setup.py clean --all
-set "SKBUILD_ARGS=-G Ninja -- -DCMAKE_C_COMPILER:PATH=icx -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
+set "CMAKE_GENERATOR=Ninja"
+set "CMAKE_ARGS=-DCMAKE_C_COMPILER:PATH=icx -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
 
-FOR %%V IN (14.0.0 14 15.0.0 15 16.0.0 16 17.0.0 17) DO @(
-  REM set DIR_HINT if directory exists
-  IF EXIST "%BUILD_PREFIX%\Library\lib\clang\%%V\" (
-     SET "SYCL_INCLUDE_DIR_HINT=%BUILD_PREFIX%\Library\lib\clang\%%V"
-  )
-)
-
-if NOT "%WHEELS_OUTPUT_FOLDER%"=="" (
-    rem Install and assemble wheel package from the build bits
-    "%PYTHON%" setup.py install bdist_wheel %SKBUILD_ARGS%
-    if errorlevel 1 exit 1
-    copy dist\mkl_umath*.whl %WHEELS_OUTPUT_FOLDER%
-    if errorlevel 1 exit 1
-) ELSE (
-    rem Only install
-    "%PYTHON%" setup.py install %SKBUILD_ARGS%
-    if errorlevel 1 exit 1
-)
+%PYTHON% -m pip install --no-build-isolation --no-deps . --verbose
+if errorlevel 1 exit 1
